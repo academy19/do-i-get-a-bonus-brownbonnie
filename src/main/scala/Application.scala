10216f4247758af71ffae40baf1f7039dd5d0eed
Bonnie Brown <bonnie.brown@digital.hmrc.gov.uk>
2019-02-20 10:47:55 +0000

    calculate bonus kata first attempt

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,11 +1,16 @@
 object Application {
 
-  // write your function here
-
+  def calculateBonus(salary: Int, ifBonus: Boolean) = {
+    ifBonus match {
+      case false => s"Your salary is £$salary"
+      case true => s"Your salary is £${(salary * 1.2).toInt}"
+    }
+  }
 
   def main(args : Array[String]) : Unit = {
     println(calculateBonus(100, true))  // should print £120
     println(calculateBonus(100, false)) // should print £100
     println(calculateBonus(99, true))   // should print £118
   }
+
 }
